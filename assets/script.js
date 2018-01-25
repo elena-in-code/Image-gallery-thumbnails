cbc0cf39536539dc71158f8dabe0827c37424675
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2018-01-25 12:50:54 +0100

    fade in animation

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,11 @@
 const [current, images, opacity] = [
     document.querySelector("#current"), 
-    document.querySelectorAll(".imgs img"), 0.4
+    document.querySelectorAll(".imgs img"), 
+    0.6
     ];
+
+//set first image the opacity 
+images[0].style.opacity = opacity;
 
 /* This is one option to make it: totally functional
 images.forEach(img => img.addEventListener("click", e => current.src = e.target.src)); */
@@ -14,6 +18,10 @@ function imgClick(e) {
     images.forEach(img => (img.style.opacity = 1));
     //Change current image
     current.src = e.target.src;
+    //add fade-in class on current image
+    current.classList.add("fade-in");
+    //Remove fade-in class
+    setTimeout(() => current.classList.remove("fade-in"), 500);
     //Change the opacity on clicked image
     e.target.style.opacity = opacity;
 }
